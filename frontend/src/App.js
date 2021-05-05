bac5eb27496f62328aa95ea09b606bcbf419be7f
alanjhuynh <alanjhuynh@gmail.com>
2021-05-04 21:25:55 -0700

    frontend init: route and components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,30 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+
+import LandingPage from './components/LandingPage';
+import ListLifts from './components/ListLifts';
+import CreateLift from './components/CreateLift';
+import UpdateLift from './components/UpdateLift';
+import LiftCard from './components/LiftCard';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path='/' component={LandingPage} />
+          <Route path='/dashboard' component={ListLifts} />
+
+        </div>
+      </Router>
+    );
+  }
+}
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +45,6 @@ function App() {
     </div>
   );
 }
+*/
 
 export default App;
