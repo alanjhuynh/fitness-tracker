5d4e3c042cec7137a819053059dcf59b6c7d4ebe
Alan Huynh <alanjhuynh@gmail.com>
2022-03-20 17:56:25 -0700

    Connect Register component to API endpoint

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,14 +1,65 @@
+import React from 'react';
+import { useState } from 'react';
+import '../App.css';
+import axios from 'axios';
+
 function Register(props) {
+
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    let onSubmit = (e) => {
+        e.preventDefault();
+
+        const data = {
+            email,
+            username,
+            password
+        }
+
+        axios
+            .post('http://localhost:8080/api/users/register', data)
+            .then(res => {
+                setEmail('');
+                setUsername('');
+                setPassword('');
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
     return (
         <div className="d-flex justify-content-center align-items-center mt-4">
             <div className="d-flex card p-4">
                 <div className="card-body">
                     <h2 className="card-title text-center">Register</h2>
-                    <form className="text-center">
+                    <form className="text-center" onSubmit={e => {onSubmit(e)}}>
                         <div className="form-group">
-                            <input type="email" className="form-control" placeholder="Email address" ></input>
-                            <input type="text" className="form-control" placeholder="Username"></input>
-                            <input type="password" className="form-control" placeholder="Password"></input>
+                            <input 
+                                type="email" 
+                                className="form-control"
+                                placeholder="Email address" 
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
+                            />
+                   
+                            <input 
+                                type="text" 
+                                className="form-control" 
+                                placeholder="Username"
+                                value={username}
+                                onChange={e => setUsername(e.target.value)}
+                            />
+                    
+                            <input 
+                                type="password" 
+                                className="form-control" 
+                                placeholder="Password"
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                            />
                         </div>
                         <div className="d-flex justify-content-end">
                             <button type="submit" className="btn btn-primary">Next</button>
